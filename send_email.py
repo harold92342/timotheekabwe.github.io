1e523107b4f35cce003cd40a4936d2d79a112d96
harold92342 <63919050+harold92342@users.noreply.github.com>
2024-05-12 20:49:29 +0200

    Update send_email.py
    Creation of the secret environment for the email API Secret

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
 
         host = "smtp.gmail.com"
         port = 465
         username = "timothekabwe@gmail.com"
-        password = "lcoo ogpb xjqa delx"
+        PASSWORD = os.getenv("PASSWORD")
         receiver = "timothekabwe@gmail.com"
         my_context = ssl.create_default_context()
 
